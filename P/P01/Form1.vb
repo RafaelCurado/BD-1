c729df52aaa855d6b4d7d43e04726b386aafd91a
David Araújo <david2araujo5@gmail.com>
2022-03-19 00:17:01 +0000

    Update

---
+++ P/P01/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+
+    End Sub
+
+    Private Sub MaskedTextBox1_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles MaskedTextBox1.MaskInputRejected, MaskedTextBox2.MaskInputRejected
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+
+    End Sub
+End Class
